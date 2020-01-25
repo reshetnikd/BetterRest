57565514201ff298a5b20cc8a310882173944183
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-25 18:41:45 +0200

    Change the UI so that it always shows recommended bedtime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -24,6 +24,24 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
+    var bedTime: String {
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return formatter.string(from: sleepTime)
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime."
+        }
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -34,37 +52,58 @@ struct ContentView: View {
                     .labelsHidden()
                     .datePickerStyle(WheelDatePickerStyle())
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                
+//                VStack(alignment: .leading, spacing: 0) {
+//                    Text("Desired amount of sleep")
+//                        .font(.headline)
+//
+//                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+//                        Text("\(sleepAmount, specifier: "%g") hours")
+//                    }
+//                }
+                Section(header: Text("Desired amount of sleep")) {
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                         Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffe intake")
-                        .font(.headline)
-                    
-                    Stepper(value: $coffeAmount, in: 1...20) {
-                        if coffeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeAmount) cups")
+//                VStack(alignment: .leading, spacing: 0) {
+//                    Text("Daily coffe intake")
+//                        .font(.headline)
+//
+//                    Stepper(value: $coffeAmount, in: 1...20) {
+//                        if coffeAmount == 1 {
+//                            Text("1 cup")
+//                        } else {
+//                            Text("\(coffeAmount) cups")
+//                        }
+//                    }
+//                }
+                Section(header: Text("Daily coffe intake")) {
+                    Picker("Number of cups", selection: $coffeAmount) {
+                        ForEach(1..<21) { number in
+                            if number == 1 {
+                                Text("1 cup")
+                            } else {
+                                Text("\(number) cups")
+                            }
                         }
                     }
+                }
+                
+                Section(header: Text("Recommended badtime")) {
+                    Text(bedTime)
+                        .font(.largeTitle)
                 }
             }
             .alert(isPresented: $showingAlert) {
                 Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
             .navigationBarTitle("BetterRest")
-            .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
+//            .navigationBarItems(trailing:
+//                Button(action: calculateBedtime) {
+//                    Text("Calculate")
+//                }
+//            )
         }
     }
     
